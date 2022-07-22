e876632fcd19c45de316cdff0febb7f864d74545
Matheus Santos <matheus.psantos2016@gmail.com>
2022-07-22 08:50:53 -0300

    feat: add colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,31 @@
 module.exports = {
   content: ["./src/**/*.{tsx,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: {
+          100: "#f1e9c9",
+          500: "#dbac2c",
+          700: "#c47f17",
+        },
+        purple: {
+          100: "#ebe5f9",
+          500: "#8047f8",
+          700: "#482995",
+        },
+        gray: {
+          50: "#fafafa",
+          100: "#f3f2f2",
+          200: "#ededed",
+          300: "#e6e5e5",
+          400: "#d7d5d5",
+          600: "#8d8686",
+          700: "#574f4d",
+          800: "#403937",
+          900: "#272221",
+        }
+      }
+    },
   },
   plugins: [],
 }
